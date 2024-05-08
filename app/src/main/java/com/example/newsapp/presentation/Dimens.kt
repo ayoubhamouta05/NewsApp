cabb72937ca22812a4c049287e783fd7ad1212d8
DELL <ayoubbhamouta@gmail?.>
2024-05-08 21:48:07 +0200

    add home screen 1/2

--- app/src/main/java/com/example/newsapp/presentation/Dimens.kt
+++ app/src/main/java/com/example/newsapp/presentation/Dimens.kt
@@ -4,9 +4,18 @@ import androidx.compose.ui.unit.dp
 
 object Dimens {
     val MediumPadding1 = 24.dp
+
     val MediumPadding2 = 30.dp
 
     val IndicatorSize = 14.dp
+
     val PageIndicatorWidth = 52.dp
 
+    val ArticleCardSize = 96.dp
+
+    val ExtraSmallPadding = 3.dp
+
+    val ExtraSmallPadding2 = 6.dp
+
+    val SmallIconSize = 11.dp
 }
--- app/src/main/java/com/example/newsapp/util/Constants.kt
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -3,5 +3,7 @@ package com.example.newsapp.util
 object Constants {
     const val USER_SETTINGS = "userSettings"
     const val APP_ENTRY = "appEntry"
+    const val API_KEY = "67c480d6c3934129b1b153bf29310d25"
 
+    const val BASE_URL = "https://newsapi.org/v2/"
 }
